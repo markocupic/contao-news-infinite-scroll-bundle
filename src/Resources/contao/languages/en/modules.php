65b70eb54a296f1f31a63295534a31538e51d87f
Marko Cupic <m.cupic@gmx.ch>
2017-11-26 17:39:15 +0100

    Bugfix in language file

--- src/Resources/contao/languages/en/modules.php
+++ src/Resources/contao/languages/en/modules.php
@@ -1,3 +1,3 @@
 <?php
 
-$GLOBALS['TL_LANG']['FMD']['contao_news_infinite_scroll'] = array('Newslist Infinite Scroll', 'Adds a list of news items by infinite scroll');
+$GLOBALS['TL_LANG']['FMD']['newslist_infinite_scroll'] = array('Newslist Infinite Scroll', 'Adds a list of news items by infinite scroll');
