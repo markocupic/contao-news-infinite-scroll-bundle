60703a83fd62d92cb5f3a7e194195a088c441e4d
Marko Cupic <m.cupic@gmx.ch>
2017-11-26 16:05:02 +0100

    dev

---
+++ src/MarkocupicContaoNewsInfiniteScrollBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Markocupic\ContaoNewsInfiniteScrollBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Configures the contao news infinite scroll bundle.
+ *
+ * @author Marko Cupic <https://github.com/markocupic>
+ */
+class MarkocupicContaoNewsInfiniteScrollBundle extends Bundle
+{
+}
